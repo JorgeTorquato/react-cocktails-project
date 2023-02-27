c42905ed95e05a47d94830a2725fc8f93cf224c9
JorgeTorquato <ellendel@gmail.com>
2023-02-27 17:38:53 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
+        obcaecati molestias autem at consequatur consequuntur cumque animi quos
+        rerum, sed asperiores odit reprehenderit. Nihil debitis explicabo
+        facilis aperiam esse magni, repellat pariatur vero aliquam, dignissimos
+        beatae eum deleniti ea ullam!
+      </p>
+    </section>
+  );
+};
+
+export default About;
